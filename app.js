2215874202b77d3e141ad06773bd38cc0bf5941b
MelaniSul <melani.sulengo@khk.ee>
2020-11-17 12:58:45 +0200

    Filter ready

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const form = document.querySelector('#task-form');
 const taskInput = document.querySelector('#task');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('#clear-tasks');
+const filterInput = document.querySelector('#filter');
 // define event listeners
 // add task to list - submit button
 form.addEventListener('submit', addTask);
 //remove task from list - fas fa-backspace icon
 taskList.addEventListener('click', removeTask);
 clearBtn.addEventListener('click', clearTasks);
+filterInput.addEventListener('keyup', filterTasks);
 
 // addTask function
 function addTask(e) {
@@ -49,4 +51,19 @@ function removeTask(e) {
 
 function clearTasks(e) {
     taskList.innerHTML = '';
+}
+
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach(
+        function (task) {
+            const item = task.firstChild.textContent.toLowerCase();
+            if (item.indexOf(text) != -1) {
+                task.style.display = 'block';
+            } else {
+                task.style.display = 'none';
+            }
+        }
+    );
+    console.log(text);
 }
